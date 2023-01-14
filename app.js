220bdc21d79662b9b4ea5263a610a91c342e4153
nithin-hunt <nithinpsgice@gmail.com>
2023-01-14 13:43:56 +0530

    routes initialise

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express();
 
 const {connectDB} = require('./config/db');
 
+const productRoutes = require('./routes/product');
+
 const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/api/v1/products", productRoutes);
 
 const start = async() => {
     try {
